13e1bac71eefe3db7028008c813a91e52b14c798
Victor Pinedo <victor.pinedo@sailpoint.com>
2023-01-15 11:55:57 -0500

    10. Grouping Actions Together with Action Types

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -2,6 +2,10 @@ import { createAction, props } from '@ngrx/store';
 import { User } from './model/user.model';
 
 export const login = createAction(
-    "[Login Page] Login Login",
+    "[Login Page] User Login",
     props<{ user: User }>()
+);
+
+export const logout = createAction(
+    "[Top Menu] Logout"
 );
