89c0207fec7bf43d89f95bf9258cd8ea56f595c6
Victor Pinedo <victor.pinedo@sailpoint.com>
2023-01-16 14:37:42 -0500

    11. NgRx Reducers - Step-by-Step Implementation

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,11 +1,25 @@
 import {
-  ActionReducerMap,
+  ActionReducerMap, createReducer, on,
 } from '@ngrx/store';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
 
-export interface State {
-
+export interface AuthState {
+  user: User
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const initialAuthState: AuthState = {
+  user: undefined
 };
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  })
+);
+
+const courses = [];
+courses.reduce()
