5dc4bdedc008d15af9e79bc3e773b9f2eeb59200
Victor Pinedo <victor.pinedo@sailpoint.com>
2023-01-23 18:20:42 -0500

    24. NgRx Metareducers - Step-by-Step Implementation

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,8 +1,20 @@
 import { routerReducer } from "@ngrx/router-store";
-import { ActionReducerMap } from "@ngrx/store";
+import { ActionReducer, ActionReducerMap, MetaReducer } from "@ngrx/store";
+import { environment } from "../../environments/environment";
 
 export interface AppState {}
 
 export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer
 };
+
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+    return (state, action) => {
+        console.log('State before: ', state);
+        console.log('Action: ', action);
+
+        return reducer(state, action);
+    }
+}
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
